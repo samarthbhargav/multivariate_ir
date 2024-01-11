6b585f1aecd356e576a3805bf240f180ab555ac7
samarthbhargav <samarth.bhargav92@gmail.com>
2024-01-11 17:40:32 +0100

    upd syntax

--- run_scripts/tas_b_zeroshot.sh
+++ run_scripts/tas_b_zeroshot.sh
@@ -6,11 +6,11 @@ DATA_CACHE_DIR=/ivi/ilps/projects/multivariate_ir/.hf_data_cache
 TOP_K=100
 BATCH_SIZE=512
 METRICS="ndcg_cut_10,map,recip_rank,recip_rank_cut_10"
-RESULTS_DIR=$MODEL_OUT/runs
+RESULTS_DIR=${MODEL_OUT}/runs
 
 
-mkdir -p $RESULTS_DIR
-mkdir -p $MODEL_OUT
+mkdir -p ${RESULTS_DIR}
+mkdir -p ${MODEL_OUT}
 
 
 ########################################################################################################################
@@ -20,62 +20,62 @@ mkdir -p $MODEL_OUT
 ## Encode MS-MARCO Passage
 echo "encoding MS-MARCO-Passage"
 python -m tevatron.driver.encode \
-  --output_dir=$TEMP_DIR \
-  --model_name_or_path $MODEL_NAME \
+  --output_dir=${TEMP_DIR} \
+  --model_name_or_path ${MODEL_NAME} \
   --fp16 \
-  --per_device_eval_batch_size $BATCH_SIZE \
+  --per_device_eval_batch_size ${BATCH_SIZE} \
   --p_max_len 128 \
   --exclude_title \
   --q_max_len 32 \
   --dataset_name Tevatron/msmarco-passage-corpus \
-  --encoded_save_path $MODEL_OUT/corpus_msmarco-passage.pkl \
-  --cache_dir $MODEL_CACHE_DIR \
-  --data_cache_dir $DATA_CACHE_DIR
+  --encoded_save_path ${MODEL_OUT}/corpus_msmarco-passage.pkl \
+  --cache_dir ${MODEL_CACHE_DIR} \
+  --data_cache_dir ${DATA_CACHE_DIR}
 
 
 
 TEST_SETS=('dev' 'dl19' 'dl20')
 for split in "${TEST_SETS[@]}"
 do
-  echo "eval $split"
+  echo "eval ${split}"
   # get embeds
   python -m tevatron.driver.encode \
-  --output_dir=$TEMP_DIR \
-  --model_name_or_path $MODEL_NAME \
+  --output_dir=${TEMP_DIR} \
+  --model_name_or_path ${MODEL_NAME} \
   --fp16 \
-  --per_device_eval_batch_size $BATCH_SIZE \
+  --per_device_eval_batch_size ${BATCH_SIZE} \
   --p_max_len 128 \
   --exclude_title \
   --q_max_len 32 \
   --encode_is_qry \
-  --dataset_name Tevatron/msmarco-passage/$split \
-  --encoded_save_path $MODEL_OUT/$split_msmarco-passage.pkl \
-  --cache_dir $MODEL_CACHE_DIR \
-  --data_cache_dir $DATA_CACHE_DIR
+  --dataset_name Tevatron/msmarco-passage/${split} \
+  --encoded_save_path ${MODEL_OUT}/${split}_msmarco-passage.pkl \
+  --cache_dir ${MODEL_CACHE_DIR} \
+  --data_cache_dir ${DATA_CACHE_DIR}
 
 
   # obtain run
   python -m tevatron.faiss_retriever \
-  --query_reps $MODEL_OUT/$split_msmarco-passage.pkl \
-  --passage_reps $MODEL_OUT/corpus_msmarco-passage.pkl \
-  --depth $TOP_K \
-  --batch_size  $BATCH_SIZE \
+  --query_reps ${MODEL_OUT}/${split}_msmarco-passage.pkl \
+  --passage_reps ${MODEL_OUT}/corpus_msmarco-passage.pkl \
+  --depth ${TOP_K} \
+  --batch_size  ${BATCH_SIZE} \
   --save_text \
-  --save_ranking_to $MODEL_OUT/$split_msmarco-passage.run
+  --save_ranking_to ${MODEL_OUT}/${split}_msmarco-passage.run
 done
 
 
-python eval_run.py --input $MODEL_OUT/dev_msmarco-passage.run \
-      --dataset msmarco-passage/dev/judged --metrics $METRICS  \
-      --output $RESULTS_DIR/dev_msmarco-passage.json
+python eval_run.py --input ${MODEL_OUT}/dev_msmarco-passage.run \
+      --dataset msmarco-passage/dev/judged --metrics ${METRICS}  \
+      --output ${RESULTS_DIR}/dev_msmarco-passage.json
 
-python eval_run.py --input $MODEL_OUT/dl19_msmarco-passage.run \
-      --dataset msmarco-passage/trec-dl-2019/judged --metrics $METRICS  \
-      --output $RESULTS_DIR/dl19.json
+python eval_run.py --input ${MODEL_OUT}/dl19_msmarco-passage.run \
+      --dataset msmarco-passage/trec-dl-2019/judged --metrics ${METRICS}  \
+      --output ${RESULTS_DIR}/dl19.json
 
-python eval_run.py --input $MODEL_OUT/dl20_msmarco-passage.run \
-      --dataset msmarco-passage/trec-dl-2020/judged --metrics $METRICS  \
-      --output $RESULTS_DIR/dl20.json
+python eval_run.py --input ${MODEL_OUT}/dl20_msmarco-passage.run \
+      --dataset msmarco-passage/trec-dl-2020/judged --metrics ${METRICS}  \
+      --output ${RESULTS_DIR}/dl20.json
 
 
 ########################################################################################################################
@@ -86,46 +86,43 @@ BATCH_SIZE=256
 
 echo "encoding SciFact corpus"
 python -m tevatron.driver.encode \
-  --output_dir=$TEMP_DIR \
-  --model_name_or_path $MODEL_NAME \
+  --output_dir=${TEMP_DIR} \
+  --model_name_or_path ${MODEL_NAME} \
   --fp16 \
-  --per_device_eval_batch_size $BATCH_SIZE \
+  --per_device_eval_batch_size ${BATCH_SIZE} \
   --p_max_len 512 \
   --dataset_name Tevatron/scifact-corpus \
-  --encoded_save_path $MODEL_OUT/corpus_scifact.pkl \
-  --cache_dir $MODEL_CACHE_DIR \
-  --data_cache_dir $DATA_CACHE_DIR \
+  --encoded_save_path ${MODEL_OUT}/corpus_scifact.pkl \
+  --cache_dir ${MODEL_CACHE_DIR} \
+  --data_cache_dir ${DATA_CACHE_DIR} \
 
 echo "eval scifact"
-
-BATCH_SIZE=256
-
 python -m tevatron.driver.encode \
---output_dir=$TEMP_DIR \
---model_name_or_path $MODEL_NAME \
+--output_dir=${TEMP_DIR} \
+--model_name_or_path ${MODEL_NAME} \
 --fp16 \
---per_device_eval_batch_size $BATCH_SIZE \
+--per_device_eval_batch_size ${BATCH_SIZE} \
 --q_max_len 64 \
 --encode_is_qry \
 --dataset_name Tevatron/scifact/dev \
---encoded_save_path $MODEL_OUT/dev_scifact.pkl \
---cache_dir $MODEL_CACHE_DIR \
---data_cache_dir $DATA_CACHE_DIR
+--encoded_save_path ${MODEL_OUT}/dev_scifact.pkl \
+--cache_dir ${MODEL_CACHE_DIR} \
+--data_cache_dir ${DATA_CACHE_DIR}
 
 
 # obtain run
 python -m tevatron.faiss_retriever \
---query_reps $MODEL_OUT/dev_scifact.pkl \
---passage_reps $MODEL_OUT/corpus_scifact.pkl \
---depth $TOP_K \
---batch_size $BATCH_SIZE \
+--query_reps ${MODEL_OUT}/dev_scifact.pkl \
+--passage_reps ${MODEL_OUT}/corpus_scifact.pkl \
+--depth ${TOP_K} \
+--batch_size ${BATCH_SIZE} \
 --save_text \
---save_ranking_to $MODEL_OUT/dev_scifact.run
+--save_ranking_to ${MODEL_OUT}/dev_scifact.run
 
 
-python eval_run.py --input $MODEL_OUT/dev_scifact.run \
-      --hf_dataset Tevatron/scifact/dev --metrics $METRICS  \
-      --output $RESULTS_DIR/dev_scifact.json
+python eval_run.py --input ${MODEL_OUT}/dev_scifact.run \
+      --hf_dataset Tevatron/scifact/dev --metrics ${METRICS}  \
+      --output ${RESULTS_DIR}/dev_scifact.json
 
 
 ########################################################################################################################
@@ -140,40 +137,40 @@ do
 
     echo "encoding corpus"
     python -m tevatron.driver.encode \
-    --output_dir=$TEMP_DIR \
-    --model_name_or_path $MODEL_NAME \
+    --output_dir=${TEMP_DIR} \
+    --model_name_or_path ${MODEL_NAME} \
     --fp16 \
-    --per_device_eval_batch_size $BATCH_SIZE \
+    --per_device_eval_batch_size ${BATCH_SIZE} \
     --p_max_len 512 \
     --dataset_name Tevatron/beir-corpus:${bds} \
-    --encoded_save_path $MODEL_OUT/corpus_bier_${bds}.pkl \
-    --cache_dir $MODEL_CACHE_DIR \
-    --data_cache_dir $DATA_CACHE_DIR
+    --encoded_save_path ${MODEL_OUT}/corpus_bier_${bds}.pkl \
+    --cache_dir ${MODEL_CACHE_DIR} \
+    --data_cache_dir ${DATA_CACHE_DIR}
 
     echo "encoding test queries"
     python -m tevatron.driver.encode \
-    --output_dir=$TEMP_DIR \
-    --model_name_or_path $MODEL_NAME \
+    --output_dir=${TEMP_DIR} \
+    --model_name_or_path ${MODEL_NAME} \
     --fp16 \
-    --per_device_eval_batch_size $BATCH_SIZE \
+    --per_device_eval_batch_size ${BATCH_SIZE} \
     --dataset_name Tevatron/beir:${bds}/test \
-    --encoded_save_path $MODEL_OUT/test_bier_${bds}.pkl \
+    --encoded_save_path ${MODEL_OUT}/test_bier_${bds}.pkl \
     --q_max_len 512 \
     --encode_is_qry \
-    --cache_dir $MODEL_CACHE_DIR \
-    --data_cache_dir $DATA_CACHE_DIR
+    --cache_dir ${MODEL_CACHE_DIR} \
+    --data_cache_dir ${DATA_CACHE_DIR}
 
     echo "obtaining run"
     python -m tevatron.faiss_retriever \
-    --query_reps $MODEL_OUT/test_bier_${bds}.pkl \
-    --passage_reps $MODEL_OUT/corpus_bier_${bds}.pkl \
+    --query_reps ${MODEL_OUT}/test_bier_${bds}.pkl \
+    --passage_reps ${MODEL_OUT}/corpus_bier_${bds}.pkl \
     --depth 1000 \
-    --batch_size $BATCH_SIZE \
+    --batch_size ${BATCH_SIZE} \
     --save_text \
-    --save_ranking_to $MODEL_OUT/bier_test_${bds}.run
+    --save_ranking_to ${MODEL_OUT}/bier_test_${bds}.run
 
 
-    python eval_run.py --input $MODEL_OUT/bier_test_${bds}.run \
-      --hf_dataset Tevatron/beir:${bds}/test --metrics $METRICS  \
-      --output $MODEL_OUT/bier_test_${bds}.json
+    python eval_run.py --input ${MODEL_OUT}/bier_test_${bds}.run \
+      --hf_dataset Tevatron/beir:${bds}/test --metrics ${METRICS}  \
+      --output ${MODEL_OUT}/bier_test_${bds}.json
 done
