94fb71b203cba223f28a69a0f32c5df30e0dd782
samarthbhargav <samarth.bhargav92@gmail.com>
2024-01-25 18:57:54 +0100

    eval tmep

--- eval_temp.py
+++ eval_temp.py
@@ -2,61 +2,63 @@ import datasets
 import pytrec_eval
 from collections import defaultdict
 import json
-import os
-import sys
 import numpy as np
 import pickle as pkl
+import argparse
 
-run_path = sys.argv[1]
-data = sys.argv[2]
-negate = sys.argv[3]
-doc_priors = sys.argv[4]
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("")
+    parser.add_argument("--run_path", required=True)
+    parser.add_argument("--data", required=True)
+    parser.add_argument("--negate", action="store_true")
+    parser.add_argument("--doc_priors", default=None)
+    parser.add_argument("--out", default=None)
 
-print("run_path", run_path)
-print("data", data)
-print("negate", negate)
-print("doc_priors", doc_priors)
+    args = parser.parse_args()
+    run_path = args.run_path
+    data = args.data
+    negate = args.negate
+    doc_priors = args.doc_priors
 
-if negate == "yes":
-    negate = -1
-else:
-    negate = 1
+    if negate:
+        negate = -1
+    else:
+        negate = 1
 
-if os.path.isfile(doc_priors):
-    print(doc_priors)
-    with open(doc_priors, "rb") as reader:
-        doc_priors = pkl.load(reader)
-else:
-    doc_priors = defaultdict(float)
-    print(f"doc priors not found at {doc_priors}. ignoring!")
+    if doc_priors:
+        print(doc_priors)
+        with open(doc_priors, "rb") as reader:
+            doc_priors = pkl.load(reader)
+    else:
+        doc_priors = defaultdict(float)
+        print(f"doc priors not found")
 
-run = defaultdict(dict)
-with open(run_path) as reader:
-    for i, line in enumerate(reader):
-        qid, doc_id, score = line.split()
-        run[qid][doc_id] = negate * float(score) + doc_priors[doc_id]
+    run = defaultdict(dict)
+    with open(run_path) as reader:
+        for i, line in enumerate(reader):
+            qid, doc_id, score = line.split()
+            run[qid][doc_id] = negate * float(score) + doc_priors[doc_id]
 
-val = datasets.load_from_disk(data)
+    val = datasets.load_from_disk(data)
 
-hf_qrel = defaultdict(dict)
-for i in range(len(val)):
-    q = val[i]
-    qid = q["query_id"]
-    hf_qrel[qid] = {}
-    for pos in q["positive_passages"]:
-        hf_qrel[qid][pos["docid"]] = 1
+    hf_qrel = defaultdict(dict)
+    for i in range(len(val)):
+        q = val[i]
+        qid = q["query_id"]
+        hf_qrel[qid] = {}
+        for pos in q["positive_passages"]:
+            hf_qrel[qid][pos["docid"]] = 1
 
-assert run.keys() == hf_qrel.keys()
+    assert run.keys() == hf_qrel.keys()
 
-measures = ["recall_5", "ndcg_cut_100", "recip_rank", "recall_1000"]
-res = pytrec_eval.RelevanceEvaluator(hf_qrel, measures).evaluate(run)
-mean_res = {}
-for measure in measures:
-    mean_ = np.mean([_[measure] for _ in res.values()])
-    mean_res[measure] = mean_
-    # print(measure, round(mean_, 4))
+    measures = ["recall_5", "ndcg_cut_100", "recip_rank", "recall_1000"]
+    res = pytrec_eval.RelevanceEvaluator(hf_qrel, measures).evaluate(run)
+    mean_res = {}
+    for measure in measures:
+        mean_ = np.mean([_[measure] for _ in res.values()])
+        mean_res[measure] = mean_
+        # print(measure, round(mean_, 4))
 
-if len(sys.argv) > 5:
-    out = sys.argv[5]
-    with open(out, "w") as writer:
-        json.dump(mean_res, writer, indent=2)
+    if args.out:
+        with open(args.out, "w") as writer:
+            json.dump(mean_res, writer, indent=2)
