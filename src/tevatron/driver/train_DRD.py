a7c6d28f4f30936d3f9fd9f1d39ddb227432024a
samarthbhargav <samarth.bhargav92@gmail.com>
2024-01-26 11:50:50 +0100

    disable_dist & MVRL args & var_token

--- src/tevatron/driver/train_DRD.py
+++ src/tevatron/driver/train_DRD.py
@@ -5,14 +5,16 @@ import sys
 import torch
 from transformers import AutoConfig, AutoTokenizer, HfArgumentParser, set_seed
 
-from tevatron.arguments import DataArguments
+from tevatron.arguments import DataArguments, MVRLTrainingArguments
 from tevatron.distillation.arguments import DistilModelArguments, DistilTrainingArguments
 from tevatron.distillation.data import DistilTrainCollator, DistilTrainDataset, HFDistilTrainDataset
 from tevatron.distillation.trainer import DistilTrainer, ListwiseDistilTrainer
 from tevatron.modeling import DenseModel
+from tevatron.modeling.dense_mvrl import MVRLDenseModel
 from tevatron.reranker.modeling import RerankerModel
 
 logger = logging.getLogger(__name__)
+
 
 def setup(rank, world_size):
     os.environ["MASTER_ADDR"] = "localhost"
@@ -25,27 +27,29 @@ def cleanup():
 
 
 def main():
-    parser = HfArgumentParser((DistilModelArguments, DataArguments, DistilTrainingArguments))
+    parser = HfArgumentParser((DistilModelArguments, DataArguments, DistilTrainingArguments, MVRLTrainingArguments))
 
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
-        model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+        model_args, data_args, training_args, mvrl_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
     else:
-        model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+        model_args, data_args, training_args, mvrl_args = parser.parse_args_into_dataclasses()
         model_args: DistilModelArguments
         data_args: DataArguments
         training_args: DistilTrainingArguments
+        mvrl_args: MVRLTrainingArguments
 
     if (
-        os.path.exists(training_args.output_dir)
-        and os.listdir(training_args.output_dir)
-        and training_args.do_train
-        and not training_args.overwrite_output_dir
+            os.path.exists(training_args.output_dir)
+            and os.listdir(training_args.output_dir)
+            and training_args.do_train
+            and not training_args.overwrite_output_dir
     ):
         raise ValueError(
             f"Output directory ({training_args.output_dir}) already exists and is not empty. Use --overwrite_output_dir to overcome."
         )
 
-    setup(rank=training_args.local_rank, world_size=torch.cuda.device_count())
+    if not training_args.disable_distributed:
+        setup(rank=training_args.local_rank, world_size=torch.cuda.device_count())
 
     # Setup logging
     logging.basicConfig(
@@ -72,16 +76,40 @@ def main():
         num_labels=num_labels,
         cache_dir=model_args.cache_dir,
     )
+
+    if mvrl_args.model_type.startswith("mvrl"):
+        assert data_args.add_var_token, "This flag has to be enabled for MVRL models"
+
     tokenizer = AutoTokenizer.from_pretrained(
         model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
         cache_dir=model_args.cache_dir,
     )
-    model = DenseModel.build(
-        model_args,
-        training_args,
-        config=config,
-        cache_dir=model_args.cache_dir,
-    )
+
+    if mvrl_args.model_type == "default":
+        model = DenseModel.build(
+            model_args,
+            training_args,
+            config=config,
+            cache_dir=model_args.cache_dir,
+        )
+    elif mvrl_args.model_type == "mvrl_no_distill":
+        raise ValueError(f"wrong model_type: {mvrl_args.model_type}")
+    elif mvrl_args.model_type == "mvrl":
+        model = MVRLDenseModel.build(
+            model_args,
+            training_args,
+            mvrl_args,
+            config=config,
+            cache_dir=model_args.cache_dir,
+        )
+    else:
+        raise NotImplementedError(mvrl_args.model_type)
+
+    if mvrl_args.model_type.startswith("mvrl"):
+        logger.info(f"adding VAR token! special tokens before: {tokenizer.all_special_tokens}")
+        tokenizer.add_special_tokens({'additional_special_tokens': ['[VAR]']}, replace_additional_special_tokens=False)
+        model.resize_token_space(len(tokenizer))
+        logger.info(f"special tokens after: {tokenizer.all_special_tokens}")
 
     teacher_config = AutoConfig.from_pretrained(
         model_args.teacher_config_name if model_args.teacher_config_name else model_args.teacher_model_name_or_path,
@@ -110,11 +138,14 @@ def main():
     )
     if training_args.local_rank > 0:
         print("Waiting for main process to perform the mapping")
-        torch.distributed.barrier()
+        if not training_args.disable_distributed:
+            torch.distributed.barrier()
+
     train_dataset = DistilTrainDataset(data_args, train_dataset.process(), tokenizer, teacher_tokenizer)
     if training_args.local_rank == 0:
         print("Loading results from main process")
-        torch.distributed.barrier()
+        if not training_args.disable_distributed:
+            torch.distributed.barrier()
 
     if training_args.listwise_kd:
         trainer = ListwiseDistilTrainer(
@@ -123,7 +154,8 @@ def main():
             args=training_args,
             train_dataset=train_dataset,
             data_collator=DistilTrainCollator(
-                tokenizer, teacher_tokenizer=teacher_tokenizer, max_p_len=data_args.p_max_len, max_q_len=data_args.q_max_len
+                tokenizer, teacher_tokenizer=teacher_tokenizer, max_p_len=data_args.p_max_len,
+                max_q_len=data_args.q_max_len
             ),
         )
     else:
@@ -133,7 +165,8 @@ def main():
             args=training_args,
             train_dataset=train_dataset,
             data_collator=DistilTrainCollator(
-                tokenizer, teacher_tokenizer=teacher_tokenizer, max_p_len=data_args.p_max_len, max_q_len=data_args.q_max_len
+                tokenizer, teacher_tokenizer=teacher_tokenizer, max_p_len=data_args.p_max_len,
+                max_q_len=data_args.q_max_len
             ),
         )
     train_dataset.trainer = trainer
@@ -142,7 +175,9 @@ def main():
     trainer.save_model()
     if trainer.is_world_process_zero():
         tokenizer.save_pretrained(training_args.output_dir)
-    cleanup()
+    if not training_args.disable_distributed:
+        cleanup()
+
 
 if __name__ == "__main__":
     main()
