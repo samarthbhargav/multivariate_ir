c921e55397676239d369b2e6ed052952cf513a7b
samarthbhargav <samarth.bhargav92@gmail.com>
2024-02-14 15:57:32 +0100

    script for QPP eval

---
+++ run_scripts/qpp_eval_model.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+MODEL_PATH_OR_NAME=$1
+MODEL_OUT=$2
+EXTRA_ARGS=$3
+LOG_FILE=$4
+
+
+if [ -z "$MODEL_PATH_OR_NAME" ] || [ -z "$MODEL_OUT" ] || [ -z "$LOG_FILE" ]
+then
+  echo "usage: MODEL_PATH_OR_NAME MODEL_OUT EXTRA_ARGS LOG_FILE"
+  exit -1
+fi
+
+
+TEST_SETS=('dl19' 'dl20')
+for split in "${TEST_SETS[@]}"
+do
+  echo "eval ${split}"
+  # get embeds
+  python -m tevatron.driver.qpp \
+  --output_dir=${MODEL_OUT} \
+  --model_name_or_path ${MODEL_PATH_OR_NAME} \
+  --qpp_save_path ${MODEL_OUT}/${split}_msmarco-passage.txt \
+  --fp16 \
+  --per_device_eval_batch_size ${BATCH_SIZE} \
+  --p_max_len 256 \
+  --exclude_title \
+  --q_max_len 32 \
+  --encode_is_qry \
+  --dataset_name Tevatron/msmarco-passage/${split} \
+  --encoded_save_path ${MODEL_OUT}/${split}_msmarco-passage.pkl \
+  ${EXTRA_ARGS} >>${LOG_FILE} 2>&1
+done
