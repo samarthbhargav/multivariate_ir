5681db287d7cc8b90c3ea843b97b046e6aa7e846
samarthbhargav <samarth.bhargav92@gmail.com>
2024-03-22 13:44:44 +0100

    added back dev

--- run_scripts/qpp_eval_model.sh
+++ run_scripts/qpp_eval_model.sh
@@ -101,8 +101,8 @@ mkdir -p ${MODEL_OUT}
 #  done
 #done
 
-#TEST_SETS=('dl19' 'dl20' 'dev')
-TEST_SETS=('dl19' 'dl20')
+TEST_SETS=('dl19' 'dl20' 'dev')
+#TEST_SETS=('dl19' 'dl20')
 for split in "${TEST_SETS[@]}"
 do
   echo "eval ${split}"
